82b2bb7d3181f1b27571bf92278bca4efa233bbc
Danny Ocean <danny.danh.nguyen@gmail.com>
2019-01-11 13:14:45 -0800

    Delete Book from List

--- app.js
+++ app.js
@@ -45,6 +45,13 @@ UI.prototype.showAlert = function(message, className) {
   }, 3000);
 }
 
+// Delete Book
+UI.prototype.deleteBook = function(target) {
+  if(target.className === 'delete') {
+    target.parentElement.parentElement.remove();
+  }
+}
+
 // Clear Fields
 UI.prototype.clearFields = function() {
   document.getElementById('title').value = '';
@@ -83,3 +90,18 @@ document.getElementById('book-form').addEventListener('submit', function(e){
 
   e.preventDefault();
 });
+
+  // Event Listener for delete
+  document.getElementById('book-list').addEventListener('click', function(e){
+    
+    // Instantiate UI
+    const ui = new UI();
+
+    // Delete book
+    ui.deleteBook(e.target);
+
+    // Show alert
+    ui.showAlert('Book Removed', 'success');
+
+    e.preventDefault();
+  });
